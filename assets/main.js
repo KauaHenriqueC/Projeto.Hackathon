3aed60782f1a92426edf1ecf478dc59035403a5f
KauaHenriqueC <kaua2hcantanhede@gmail.com>
2026-06-20 16:44:05 -0300

    [FEAT]: Cards in html, started main.js, style.css cursor changed to pointer and display none [REFACTOR]: Rename class in html

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+// Variables
+
+// cards
+var card1 = document.getElementsByClassName('card1') // Card of aba 1
+var card2 = document.getElementsByClassName('card2') // Card of aba 2
+var card3 = document.getElementsByClassName('card3') // Card of aba 3
+
+// Abas
+
+var aba1 = document.getElementById('a1')
+var aba2 = document.getElementsByClassName('a2')
+var aba3 = document.getElementsByClassName('a3')
+
+// Events
+
+aba1.addEventListener("click", () => {
+    console.log('oioi')
+}
+)
+// Functions
+
+// Function abas
+
